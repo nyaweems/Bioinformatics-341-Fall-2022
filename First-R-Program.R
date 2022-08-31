0059840962c723df9452ea523dd36f99466e6c17
Nya Weems <111668559+nyaweems@users.noreply.github.com>
2022-08-31 09:39:38 -0400

    Add files via upload

--- First-R-Program.R
+++ First-R-Program.R
@@ -8,3 +8,22 @@ BiocManager::install("affy")
 #Load the package affy
 library(affy)
 
+#Read CEL microarray files in R using library affy
+
+#Setting the working directory
+setwd("/Users/nyaweems/Desktop")
+
+#Read the CEL files from current working directory 
+eset <- ReadAffy()
+
+#Extracting expression values using following function
+rma <- exprs(eset)
+
+#Boxplot to view the expression of samples
+boxplot(rma)
+
+
+
+
+
+
