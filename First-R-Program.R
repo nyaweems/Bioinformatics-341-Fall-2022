b60eb2386bd16eb99759bba09e87d98bbb57e2aa
Nya Weems <111668559+nyaweems@users.noreply.github.com>
2022-09-03 17:51:40 -0400

    Add files via upload

--- First-R-Program.R
+++ First-R-Program.R
@@ -5,7 +5,11 @@ if (!require("BiocManager", quietly = TRUE))
 
 BiocManager::install("affy")
 
-#Load the package affy
+
+
+
+
+c#Load the package affy
 library(affy)
 
 #Read CEL microarray files in R using library affy
@@ -14,13 +18,29 @@ library(affy)
 setwd("/Users/nyaweems/Desktop")
 
 #Read the CEL files from current working directory 
-eset <- ReadAffy()
+eset <- ReadAffy("GSM18423_PA-D_132.cel")("GSM18422_PA-D_102.cel")("GSM18425_PA-D_216.cel")("GSM18426_PA-D_225.cel")("GSM18424_PA-D_206.cel")
 
 #Extracting expression values using following function
 rma <- exprs(eset)
 
 #Boxplot to view the expression of samples
 boxplot(rma)
+
+#Pre-processing/Normalization of the chips: Mas5.0, RMA, Quantile Normalization
+
+#Install package oligo and rlang
+if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager");BiocManager::install("rlang")
+if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager");BiocManager::install("oligo")
+
+#Applying RMA normalization function 
+library(rlang)
+library(oligo)
+celFiles = list.celfiles("/Users/nyaweems/Desktop")
+affyRaw <-read.celfiles(celFiles)
+eset <- rma(affyRaw)
+boxplot(eset)
+
+
 
 
 
