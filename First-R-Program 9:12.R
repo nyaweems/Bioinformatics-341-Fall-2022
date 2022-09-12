66c1840f5db4dac75f57e31c2f3e80e141a34d34
Nya Weems <111668559+nyaweems@users.noreply.github.com>
2022-09-12 09:43:46 -0400

    Add files via upload

---
+++ First-R-Program 9:12.R
@@ -0,0 +1,59 @@
+#Author: Weems;Datw:08/29/2022;Purpose: Teszting R Script
+#Install microarray packages
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("affy")
+
+
+
+
+
+#Load the package affy
+library(affy)
+
+#Read CEL microarray files in R using library affy
+
+#Setting the working directory
+setwd("/Users/nyaweems/Desktop")
+
+#Read the CEL files from current working directory 
+eset <- ReadAffy("GSM18423_PA-D_132.cel")("GSM18422_PA-D_102.cel")("GSM18425_PA-D_216.cel")("GSM18426_PA-D_225.cel")("GSM18424_PA-D_206.cel")
+
+#Extracting expression values using following function
+rma <- exprs(eset)
+
+#Boxplot to view the expression of samples
+boxplot(rma)
+
+#Pre-processing/Normalization of the chips: Mas5.0, RMA, Quantile Normalization
+
+#Install package oligo and rlang
+if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager");BiocManager::install("rlang")
+if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager");BiocManager::install("oligo")
+
+#Applying RMA normalization function 
+library(rlang)
+library(oligo)
+celFiles = list.celfiles("/Users/nyaweems/Desktop")
+affyRaw <-read.celfiles(celFiles)
+eset <- rma(affyRaw)
+boxplot(eset)
+
+rma_normalized <- exprs(eset)
+Fold_change <- rma_normalized[,1] - rma_normalized[,2]
+
+
+# Significant Threshold (up-regulated) >1.5 and (Down-regulated) <-1.5
+
+Fold_change <- as.data.frame(Fold_change)
+Up_regulated <- Fold_change[Fold_change > 1.5,]
+Down_regulated <- Fold_change[Fold_change < -1.5,]
+
+
+
+
+
+
+
+
